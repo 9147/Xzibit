65d92abb6d754618854787627ac5fc41fe5dfa8f
Jayesh <uramanattijayesh@gmail.com>
2023-06-08 09:46:58 +0530

    data presentation updates

--- fakeNewsDetector/home/webScraper.py
+++ fakeNewsDetector/home/webScraper.py
@@ -2,30 +2,46 @@
 from gnews import GNews
 from bs4 import BeautifulSoup
 import requests
+from googletrans import Translator, constants
+from pprint import pprint
 
 def process_news_object(url, content):
 
     # print("Source URL:", url)
     # print("News Content:", content)
-    # print(url)  # Print an empty line for separation
+    # print()  # Print an empty line for separation
 
     # ... continue with your scraping code ...
 
     cleaned_content = content.replace('\r', '').replace('\n', '')
     clean_content=cleaned_content
     google_news = GNews()
-    Indian_news = google_news.get_news(clean_content)
-    if Indian_news:
-        news = Indian_news[0]
-        # print("Title : ", news["title"][:news["title"].rindex('-')], "\nPublisher : ", news["publisher"]["title"], "\nSource : ", news["url"])
-        url_search = news['url']
+    Similar_news = google_news.get_news(clean_content)
     
-        code = requests.get(url_search)
+    if Similar_news:
+        news = Similar_news[0]
+        # print("Title : ", news["title"][:news["title"].rindex('-')], "\nPublisher : ", news["publisher"]["title"], "\nSource : ", news["url"])
+
+        print("news URL : ", news['url'])
+        
+        code = requests.get(news['url'])
         soup = BeautifulSoup(code.text, "html.parser")
     
         print("\nDetails :")
 
+        totalText = []
         for item in soup.find_all('p'):
             print(item.text)
+            totalText.append(item.text)
+            
+        # for item in soup.find_all('span'):
+        #     print(item.text)
+        #     totalText.append(item.text)
+        
+        for item in soup.find_all('div'):
+            print(item.text)
+            totalText.append(item.text)
+        
 
-        return soup.find_all('p')
+        # return soup.find_all('p')
+        return totalText
